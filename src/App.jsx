2ef7358a5b7da6c2a1a2dc2e1458762fbc6f581a
Yuji Imagawa <ug23@users.noreply.github.com>
2022-01-03 22:30:39 +0900

    P132 CSS Moduleを使う

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import { useState } from "react";
-import { ColoredMessage } from "./components/ColoredMessage";
+import { CssModules } from "./CssModules";
 
 export const App = () => {
-  console.log("レンダリング");
-
-  const [num, setNum] = useState(0);
-  const onClickButton = () => {
-    setNum((prev) => prev + 1);
-  };
-
   return (
     <>
-      <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
-      <ColoredMessage color="pink">元気です！</ColoredMessage>
-      <button onClick={onClickButton}>ボタン</button>
-      <p>{num}</p>
+      <CssModules />
     </>
   );
 };
