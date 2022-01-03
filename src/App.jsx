2ac4a1bb5d4d9027f2ca315f517b0aacdd2f4ea7
Yuji Imagawa <ug23@users.noreply.github.com>
2022-01-03 16:59:10 +0900

    P104 styleはJSのオブジェクトで渡す JSを書くためにbraceでくくり、その中にJSONを書くので二重braceになる

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export const App = () => {
 
   return (
     <>
-      <h1>こんにちは！</h1>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
       <p>お元気ですか？</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
