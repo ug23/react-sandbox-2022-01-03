8fbd735261d02efd41d968ef149e800571569e84
Yuji Imagawa <ug23@users.noreply.github.com>
2022-01-03 12:45:06 +0900

    Fragmentコンポーネントにしてもいい

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+import { Fragment } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
   return (
-    <>
+    <Fragment>
       <h1>こんにちは！</h1>
       <p>お元気ですか？</p>
-    </>
+    </Fragment>
   );
 };
 
