4759654c3e867d5b10adfa83431ea633a67e0407
Yuji Imagawa <ug23@users.noreply.github.com>
2022-01-03 12:42:47 +0900

    P98 Appで返すのは単一のタグである必要がある l.12のrenderによってhtmlのdiv#idにAppタグを置く この要素はl.3のAppが返す単一のタグとなる

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか？</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
