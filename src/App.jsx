be03926772193d7bff77895100f113f5fbbf47fd
Yuji Imagawa <ug23@users.noreply.github.com>
2022-01-03 21:42:18 +0900

    P119 useStateでカウンタ機能を作る setNum() に値を渡すとnumが更新されるのはレンダリングされたあとになる setNumでnumを更新したければsetNumに関数を渡す

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
 
 export const App = () => {
+  const [num, setNum] = useState(0);
   const onClickButton = () => {
-    alert();
+    setNum(num + 1);
   };
 
   return (
@@ -11,6 +13,7 @@ export const App = () => {
       <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
       <ColoredMessage color="pink">元気です！</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 };
